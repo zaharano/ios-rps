62626cdf8b7eda1dbce31418ba0704485d8ea88d
Zach <zraisnorman@gmail.com>
2024-08-15 11:00:45 -0400

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Zachary Rais-Norman on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
